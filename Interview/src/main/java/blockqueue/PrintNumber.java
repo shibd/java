5e9d2137d3412557f3713b0b95ec4160f691727d
bdshi <bdshi@dfocuspace.com>
2019-09-22 16:12:24 +0800

    feat: 修改轮训打印,使用一个条件变量即可完成

--- Interview/src/main/java/blockqueue/PrintNumber.java
+++ Interview/src/main/java/blockqueue/PrintNumber.java
@@ -1,9 +1,5 @@
 package blockqueue;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * @author: baozi
  * @date: 2019/9/19 16:50
@@ -14,30 +10,27 @@ public class PrintNumber extends Thread {
     // 共享变量
     private static int startNum = 1;
 
-    // 自己满足的条件变量
-    private Condition my;
-    // 其他人满足的条件变量
-    private Condition other;
     // 同步锁
-    private Lock lock;
+    private Object lock;
     // 条件变量
     private String type;
 
-    public PrintNumber(Lock lock, Condition my, Condition other, String type) {
+    public PrintNumber(Object lock, String type) {
         this.lock = lock;
-        this.my = my;
-        this.other = other;
         this.type = type;
     }
 
     public static void main(String[] args) {
-        Lock lock = new ReentrantLock();
-        Condition ji = lock.newCondition();
-        Condition ou = lock.newCondition();
-        new PrintNumber(lock, ji, ou, "odd").start();
-        new PrintNumber(lock, ou, ji, "even").start();
+        Object lock = new Object();
+        new PrintNumber(lock, "odd").start();
+        new PrintNumber(lock, "even").start();
     }
 
+    /**
+     * 条件变量方法
+     *
+     * @return
+     */
     private boolean isMe() {
         return (type.equals("odd") && startNum % 2 == 1) || (type.equals("even") && startNum % 2 == 0);
     }
@@ -45,20 +38,17 @@ public class PrintNumber extends Thread {
     @Override
     public void run() {
         try {
-            lock.lock();
-            while (startNum < 100) {
-                while (!isMe()) {
-                    my.await();
+            synchronized (lock) {
+                while (startNum < 100) {
+                    while (!isMe()) {
+                        lock.wait();
+                    }
+                    System.out.println(startNum);
+                    startNum++;
+                    lock.notifyAll();
                 }
-                System.out.println(startNum);
-                startNum++;
-                other.signalAll();
             }
         } catch (Exception e) {
-
-        } finally {
-            lock.unlock();
         }
-
     }
 }
